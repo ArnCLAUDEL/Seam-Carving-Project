9a7d35e990c760eb54cfb45a52b5f159b6ecf3c4
Arnaud CLAUDEL <arnaud-claudel@hotmail.fr>
2017-10-11 16:22:30 +0200

    Addition of a test button

--- gui/frame.py
+++ gui/frame.py
@@ -17,6 +17,10 @@ class Frame:
 
         self.label = tkinter.StringVar()
         self.label.set("NONE")
+
+        self.test_button = tkinter.Button(self.frame, text="Test", command=self.energyTest)
+        self.test_button.pack()
+        
         self.current_file_label = tkinter.Label(self.frame, textvariable=self.label)
         self.current_file_label.pack()
 
@@ -26,7 +30,11 @@ class Frame:
         if file:
             print("Picture selected")
             self.core.setImage(file)
+            print("Picture loaded")
             self.updateLabel()
 
     def updateLabel(self):
         self.label.set(self.core.image.path)
+
+    def energyTest(self):
+        self.core.energyOf(2,3)
