1f2142e1bcc63fbe73f2e87d9d6630c36b804d84
Arnaud <arnaud-claudel@hotmail.fr>
2017-11-20 11:51:32 +0100

    method references in local variables

--- model/energyComputer.py
+++ model/energyComputer.py
@@ -30,21 +30,20 @@ class EnergyComputer:
     def stupid_seam_finder(self):
         pe = {"seam_energy":math.inf, "path":[]}
         start1,end1 = 0,0
-        start2, end2= 0, 0
+        start2,end2= 0, 0
+        path = []
+        fit, e_min = self.fitToGrid, self.energy_min
         for i in range(1, self.image.w -2):
-            x = i-1
-            seam_energy = 0
-            path = []
+            x, seam_energy, path= i-1, 0, []
             for j in range(1, self.image.h -1):
                 y = j
-                c,e = self.energy_min(x, y)
+                c,e = e_min(x, y)
                 seam_energy += e
                 path.append(c)
                 x = c[0]
-                x = self.fitToGrid(x,y)[0]
+                x = fit(x,y)[0]
             if pe["seam_energy"] > seam_energy:
-                pe["seam_energy"] = seam_energy
-                pe["path"] = path
+                pe = {"seam_energy":seam_energy,"path":path}
         print(end1-start1," + ",end2 - start2)
         return pe
 
@@ -54,6 +53,7 @@ class EnergyComputer:
     def energy_min(self, x, y):
         e1, e2, e3 = self.energy(x - 1, y), self.energy(x, y), self.energy(x + 1, y)
         e = min(e1, e2, e3)
+        # c = (x + 1, y) if e == e3 else ((x, y) if e == e2 else (x - 1, y))
         if e == e3:
             c = (x + 1, y)
         elif e == e2:
@@ -85,7 +85,7 @@ class EnergyComputer:
         x2,y2 = x +c[0],y +c[1]
 
         res = self.intensityComputed.get((x2,y2),-1)
-        if res < 0 :
+        if res < 0:
             res = intensity(self.image.get(x2, y2))
             self.intensityComputed[(x2, y2)] = res
         return res
