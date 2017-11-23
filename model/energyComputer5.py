ec460bdcb4c4a1543b0a68008da24d3c936095ca
Arnaud <arnaud-claudel@hotmail.fr>
2017-11-23 12:38:39 +0100

    incomplete horizontal and vertical seam finder

--- model/energyComputer5.py
+++ model/energyComputer5.py
@@ -32,38 +32,54 @@ class EnergyComputer:
 
     @timer
     def stupid_seam_finder(self):
+
+        #path_energy: minimal path
         pe = {"seam_energy":math.inf, "path":[]}
+
+        #timers
         start1,end1,start2,end2 = float(0),float(0),float(0),float(0)
-        fit, e_min, energy = self.fitToGrid, self.energy_min, self.energy
+
+        #functions calls in local variables for better efficiency
+        fit, energy = self.fitToGrid, self.energy
+
+        #width and height in local variables for better efficiency
         w,h = self.image.w, self.image.h
+
+        #number of energies computed
         self.count = 0
-        for i in range(1, self.image.w -2):
-            x, seam_energy, path = i-1, 0, []
+
+        #variables used to work around a pixel, left/right or up/down
+        iX1,iY1, iX2,iY2, iX3,iY3 = -1,0, 0,0, 1,0
+
+        #index for (x,y) tuple
+        iC = 0
+
+        #function to keep a coordinate in bound
+        fit = lambda x:1 if x <= 0 else (self.image.w - 3 if x >= self.image.w - 3 else x)
+
+        for i in range(0, w -2):
+            x = i
+            #current energy and path
+            seam_energy, path = 0, []
             append = path.append
+
             for j in range(1, h-1):
                 y = j
-                e1, e2, e3 = energy(x - 1, y), energy(x, y), energy(x + 1, y)
+                e1, e2, e3 = energy(x +iX1, y +iY1), energy(x +iX2,y +iY2), energy(x +iX3, y +iY3)
                 e = min(e1, e2, e3)
-                c = (x + 1, y) if e == e3 else ((x, y) if e == e2 else (x - 1, y))
+                c = (x +iX3, y +iY3) if e == e3 else ((x +iX2,y +iY2) if e == e2 else (x +iX1, y +iY1))
                 seam_energy += e
                 append(c)
-                x = c[0]
-                x = 1 if x <= 0 else (w - 3 if x >= w - 3 else x)
+                x = c[iC]
+                x = fit(x)
+
             if pe["seam_energy"] > seam_energy:
                 pe = {"seam_energy":seam_energy,"path":path}
+
         print(self.end - self.start)
         print(end1-start1,"+",end2-start2)
         print(len(self.energyComputed), self.count)
         return pe
-
-    """
-        +150ms
-    """
-    def energy_min(self, x, y):
-        e1, e2, e3 = self.energy(x - 1, y), self.energy(x, y), self.energy(x + 1, y)
-        e = min(e1, e2, e3)
-        c = (x + 1, y) if e == e3 else ((x, y) if e == e2 else (x - 1, y))
-        return c,e
 
     def energy(self, x, y):
         if x == 0 or y == 0:
@@ -74,7 +90,6 @@ class EnergyComputer:
             pass
 
         gx, gy = self.g3(x, y, self.__gx_coords), self.g3(x, y, self.__gy_coords)
-
         res = math.sqrt(gx*gx + gy*gy)
         self.energyComputed[(x,y)] = res
         return res
@@ -104,8 +119,9 @@ class EnergyComputer:
             v.append(res)
         return v[0] + v[1] * 2 + v[2] - v[3] - v[4] * 2 - v[5]
 
-    def removeVerticalSeam(self, path, iX=1, iY=0):
+    def removeVerticalSeam(self, path, c=(1,0)):
         energy = self.energy
+        iX,iY = c[0], c[1]
         for (x,y) in path:
             for x2 in range(x,self.image.w-2):
                 self.energyComputed[(x,y)] = energy(x+iX,y+iY)
