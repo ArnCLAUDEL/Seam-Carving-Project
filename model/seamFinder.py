0a2ad27c564fbf991c7bc1ed7f6a0635eb667d26
Arnaud <arnaud-claudel@hotmail.fr>
2018-01-08 14:21:27 +0100

    small refactor

--- model/seamFinder.py
+++ model/seamFinder.py
@@ -12,10 +12,7 @@ class SeamFinder:
         # path_energy: minimal path
         pe = {"seam_energy":math.inf, "path":[]}
 
-        # width and height in local variables for better efficiency
-        w,h = self.image.w, self.image.h
-
-        pe = self.compute_vertical(pe, w, h)
+        pe = self.find_vertical_seams(pe)
 
         return pe
 
@@ -23,20 +20,30 @@ class SeamFinder:
     # pe : [path,energy]
     # w : width of the image
     # h : height of the image
-    def compute_vertical(self, pe, w, h):
+    def find_vertical_seams(self, pe):
+        for x in range(0, self.image.w -2):
+            pe = self.find_vertical_seam(x, pe)
+        return pe
+
+    def find_vertical_seam(self, x, pe):
+        # width and height in local variables for better efficiency
+        w, h = self.image.w, self.image.h
+
+        # energies in a local variable for better efficiency
         energy_computed = self.energyCalculator.energyComputed
-        for x in range(0, w -2):
-            seam_energy, path = 0, []
-            append = path.append
-            for y in range(1, h-1):
-                e1, e2, e3 = energy_computed[x - 1][y + 1], energy_computed[x][y + 1], energy_computed[x + 1][y + 1]
-                e = min(e1, e2, e3)
-                x = x +1 if e == e3 else x if e == e2 else x -1
-                seam_energy += e
-                append((x,y))
-                x = 1 if x <= 0 else (w - 3 if x >= w - 3 else x)
-            if pe["seam_energy"] > seam_energy:
-                pe = {"seam_energy": seam_energy, "path": path}
+
+        seam_energy, path = 0, []
+        append = path.append
+
+        for y in range(1, h-1):
+            e1, e2, e3 = energy_computed[x - 1][y + 1], energy_computed[x][y + 1], energy_computed[x + 1][y + 1]
+            e = min(e1, e2, e3)
+            x = x if e == e2 else x + 1 if e == e3 else x - 1
+            seam_energy += e
+            append((x,y))
+            x = 1 if x <= 0 else (w - 3 if x >= w - 3 else x)
+        if pe["seam_energy"] > seam_energy:
+            pe = {"seam_energy": seam_energy, "path": path}
         return pe
 
     def remove_vertical_seam(self, path):
