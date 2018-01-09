dc7494ffe272c81fb44639351c855c95a530084f
Arnaud <arnaud-claudel@hotmail.fr>
2018-01-09 23:29:22 +0100

    more doc

--- model/seamFinder.py
+++ model/seamFinder.py
@@ -6,12 +6,35 @@ class SeamFinder:
 
     def __init__(self, image):
         self.image = image
-        self.energyCalculator = ec.EnergyCalculator(image)
-        self.grid = [[(math.inf, (-1, -1)) for y in range(self.image.h)] for x in range(self.image.w)]
-        self.previous_avg_x = 0
-        self.preprocess()
 
-    def preprocess(self):
+        # Object that will provide the energies.
+        self.energyCalculator = ec.EnergyCalculator(image)
+
+        # Grid that will contain, for each pixel, a tuple (seam_energy, (x2,y2)).
+        # seam_energy : The energy of the lowest-cost seam that ends to this pixel.
+        # (x2,y2) : The upper pixel that belongs to the lowest-cost seam.
+        # The grid has the following shape : (width,height).
+        self.grid = [[(math.inf, (-1, -1)) for y in range(self.image.h)] for x in range(self.image.w)]
+
+        # Variable used to reduce the amount of computation
+        # This variable is updated, each time a new minimal seam is computed.
+        # It represents the average of the x-part of each pixel coordinate (x,y) that belongs to the seam.
+        # @check SeamFinder.compute_paths documentation for more details.
+        self.previous_avg_x = 0
+
+    # Compute, for each pixel, the next pixel to follow to build the lowest-cost vertical seam.
+    # This algorithm uses dynamic programming to compute it in a reasonable delay.
+    # Pixels in the first row are initialized just with their energies and dummy coordinates.
+    #  Starting from the second line, each pixel will look at his 3 upper pixels (top-right, top, top-left).
+    # - and will choose the one with the lowest-cost.
+    # For easiness reasons, we do not compute any pixel in the most-left/right columns.
+    # This algorithm is greedy and uses a variable range for his computations.
+    # Therefore when a new seam computation occurs, we only re-compute a small area around this coordinate.
+    # This is a greedy choice, but it gives a reasonable result and avoids a lot of useless computations.
+    # @check SeamFinder.__avg_x_range for nore details.
+    # ! Time consuming operation.
+    def compute_paths(self):
+
         # energies in a local variable for better efficiency
         energy_computed = self.energyCalculator.energyComputed
         grid = self.grid
@@ -19,8 +42,6 @@ class SeamFinder:
         # Initialization of the first row
         for x in self.avg_x_range():
             grid[x][0] = (energy_computed[x][0], (-1,-1))
-
-        #print(self.avg_x_range(), len(grid), len(grid[len(grid)-1]), grid[len(grid)-2][100])
 
         for y in range(1, self.image.h - 1):
             for x in self.avg_x_range():
@@ -31,9 +52,19 @@ class SeamFinder:
 
                 # We retrieve the coordinates of the lowest energy
                 (x2, y2) = (x, y - 1) if e == e2 else (x + 1, y - 1) if e == e3 else (x - 1, y - 1)
+
+                # Data update
                 grid[x][y] = (e + energy_computed[x][y], (x2, y2))
 
-
+    # Return a greedy Range object used in path computation.
+    # The Range returned depends on the previous computed seam.
+    # @check SeamFinder.previous_avg_x for more details.
+    # If the x-average is 0 - it usually means that it's the first computation
+    # - then we returned a classic range(1,width).
+    # Else, we make an area around the x-coordinate that represents the pixels which are the most-likely
+    # - to change one, or more, seam.
+    # In addition, we re-compute the two most-right columns to make sure that they won't lead us to a
+    # - non-existent pixel.
     def avg_x_range(self):
         if self.previous_avg_x == 0:
             return range(1, self.image.w-1)
@@ -63,17 +94,24 @@ class SeamFinder:
         pe = self.find_vertical_seams(pe)
         return pe
 
-    # Find a vertical seam in the image
-    # pe : [path,energy]
-    # w : width of the image
-    # h : height of the image
-    # Time consuming operation
+    # Find and return a low-energy seam.
+    # Return a dictionary {"seam_energy", "path"}.
+    # seam_energy : The energy of the seam found.
+    # path : a list of (x,y) coordinates.
+    # ! Time consuming operation
     @timer
     def find_vertical_seams(self, pe):
-        self.preprocess()
+        self.compute_paths()
         return self.find_vertical_seam()
 
 
+    # Find and return a low-energy seam.
+    # It loops over the pixels in the most-bottom row and takes the pixel with the lowest seam energy.
+    # Then it climb the grid and add each pixel to a list. After computation, the list is reversed.
+    # This will also update the average x-coordinate. @check SeamFinder.previous_avg_x for more details.
+    # Return a dictionary {"seam_energy", "path"}.
+    # seam_energy : The energy of the seam found.
+    # path : a list of (x,y) coordinates, from top to bottom.
     @timer
     def find_vertical_seam(self):
         # width and height in local variables for better efficiency
@@ -108,6 +146,13 @@ class SeamFinder:
 
         return {"seam_energy": energy, "path": path}
 
+    # Remove every pixel from the path in the grid.
+    # Since the grid is shaped like [column][row], it shifts pixels instead of just popping pixels for each row.
+    # If the seam is closer to the right-side of the image, then pixels on the right-side of the seam
+    #   - are shifted to the left.
+    # Else, pixels on the left-side of the seam is shifted to the right.
+    # This is computed with the average x-coordinate. @check SeamFinder.previous_avg_x for more details.
+    # This technique, in most cases, should not shift more than half of the pixels.
     @timer
     def remove_vertical_seam(self, path):
         if self.previous_avg_x > self.image.w // 2:
